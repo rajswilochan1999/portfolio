4f1ffa531d52ef908f772b36bc105d6bb367b815
rajswilochan1999 <rajswil.ug18.cs@nitp.ac.in>
2021-03-20 13:18:24 +0530

    Adding existing files 1

--- js/main.js
+++ js/main.js
@@ -14,3 +14,40 @@ $btns.click(function(e){
 
 
 });
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function my_cv_link() { 
+    window.open("https://docs.google.com/document/d/1aKSk62cwiFZCprMBz-n1UTsoPLhZSeHl35eG_GaHv2s/edit"); 
+} 
+
+  
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+  localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark'); localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+}
+// Immediately invoked function to set the theme on initial load
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+})
